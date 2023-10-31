e183c3625112ed54a3bd84ef8ae0b52ab984e421
RepoRover <web.yugaitrov@gmail.com>
2023-10-31 04:29:40 +0200

    Fix deployed env

--- server/src/config.js
+++ server/src/config.js
@@ -1,8 +1,16 @@
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import fs from 'fs';
 
 import dotenv from 'dotenv';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-dotenv.config({ path: `${__dirname}/../../.env` });
+const filePath = `${__dirname}/../../.env`;
+
+if (fs.existsSync(filePath)) {
+	dotenv.config({ path: `${__dirname}/../../.env` });
+} else {
+	const cutPoint = __dirname.indexOf('actions-runner');
+	dotenv.config({ path: `${__dirname.substring(0, cutPoint)}/env/rulers.env` });
+}
