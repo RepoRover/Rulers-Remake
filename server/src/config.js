7fd4526846841091fa020f96fd7d27a2cff49977
RepoRover <web.yugaitrov@gmail.com>
2023-11-01 00:28:18 +0200

    Fix config

--- server/src/config.js
+++ server/src/config.js
@@ -11,6 +11,6 @@ const filePath = `${__dirname}/../../.env`;
 if (fs.existsSync(filePath)) {
 	dotenv.config({ path: `${__dirname}/../../.env` });
 } else {
-	const cutPoint = __dirname.indexOf('actions-runner');
+	const cutPoint = __dirname.indexOf('rulers-runner');
 	dotenv.config({ path: `${__dirname.substring(0, cutPoint)}/env/rulers.env` });
 }
